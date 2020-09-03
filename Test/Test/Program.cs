4473a0581c50842cdcfc6d17e630ed842be62f96
qusehdgns <qusehdgns123@gmail.com>
2020-09-03 23:10:07 +0900

    C#

---
+++ Test/Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("컴퓨터시스템과");
+            Console.WriteLine("201645017");
+            Console.WriteLine("변동훈");
+        }
+    }
+}
