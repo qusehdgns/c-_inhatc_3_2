6212d3b440ec5611a68b9f9148a7e02319109c2b
qusehdgns <qusehdgns123@gmail.com>
2020-11-16 22:57:10 +0900

    dong

--- A_Ban/Editor/Editor/Form1.cs
+++ A_Ban/Editor/Editor/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,96 @@ namespace Editor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string filepath = "";
+        string copyfield = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "텍스트 파일 (*.txt)|*.txt|모든 파일 (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filepath = openFileDialog1.FileName;
+
+                StreamReader sr = new StreamReader(filepath);
+
+                string result = sr.ReadToEnd();
+
+                textBox1.Text = result;
+                sr.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "텍스트 파일 (*.txt)|*.txt";
+
+            if (textBox1.Text.Equals("") != true)
+            {
+                if (filepath.Equals("") == true)
+                {
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        filepath = saveFileDialog1.FileName;
+
+                        SaveFile();
+                    }
+                }
+                else
+                {
+                    SaveFile();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filepath = saveFileDialog1.FileName;
+
+                SaveFile();
+            }
+        }
+
+        private void SaveFile()
+        {
+            StreamWriter sw;
+            sw = File.CreateText(filepath);
+
+            sw.WriteLine(textBox1.Text);
+            sw.Close();
+
+            textBox1.Text = "";
+
+            filepath = "";
+
+            Form2 f = new Form2();
+
+            f.Show();
+
+            textBox1.Font = DefaultFont;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Font = fontDialog1.Font;
+                textBox1.ForeColor = fontDialog1.Color;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            copyfield = textBox1.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "\r\n" + copyfield;
         }
     }
 }
---
+++ A_Ban/Editor/Editor/Form2.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Editor
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(49, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "저장 완료";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 103);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
